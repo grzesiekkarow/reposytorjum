dc8d4584035f03bf9bc2f7b58c6c06bc642aa4e2
grzesiekkarow <44462067+grzesiekkarow@users.noreply.github.com>
2018-11-08 10:15:04 +0100

    Add files via upload
    dodane dodawanie

--- HelloWorld.c
+++ HelloWorld.c
@@ -2,6 +2,22 @@
 int main()
 {
     printf("Hello World");
+    int a, b;
+    int wynik;
+    char c;
+    printf(" Podaj dwie liczby: ");
+    scanf("%i", &a);
+    scanf("%i", &b);
+    printf("Podaj dzialanie (+, -, *, /, p- pierwiastek, ^): ");
+    scanf("%c,&c");
+    switch(c)
+    {
+    case '+':
+        wynik=a+b;
+        break;
+    default:
+        break;
+    }
     getchar();
     return 0;
 }
